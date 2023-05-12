bfe87ade09b036f785e68c52ac64c8f91e28325c
SujarHada <sujarhadaofficail@gmail.com>
2023-05-12 18:01:32 +0545

    add/view/edit/delete function in single class.

--- lists/urls.py
+++ lists/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('',views.index.as_view(),name="home"),
-    path('todo/<int:id>/view', views.view.as_view(), name='view'),
-    path('todo/',views.add.as_view(),name="add"),
-    path('todo/<int:id>/delete',views.delete.as_view(),name="delete"),
-    path('todo/<int:id>/edit',views.edit.as_view(),name="edit"),
+    path('' , views.God().index, name="index"),
+    path('todo/', views.God().add , name="add"),
+    path('todo/<int:id>/edit', views.God().edit , name="edit"),
+    path('todo/<int:id>/delete', views.God().delete , name="delete"),
+  
     # path('edit_it/<int:id>',views.edit_it.as_view(),name="edit_it")
 
     
--- lists/views.py
+++ lists/views.py
@@ -1,56 +1,141 @@
 from django.shortcuts import render , redirect, HttpResponse
 from lists.models import Task
 from django.views import View
+from django.contrib import messages
+
 
 # Create your views here.
-class index(View):
-    def get(self,request):
+# class Home(View):
+    
+class God():
+ 
+    def index(self,request):
         tasks = Task.objects.all()
+       
         return render(request,'index.html',{'tasks':tasks})
 
-class add(View):
-    def get(self,request):
-     return render(request,'add.html')  
-    
-    def post(self,request):
+
+    # Add method / Post
+    def add(self,request):
         if request.method=="POST":
             tname = request.POST.get('tname')
             ddate = request.POST.get('ddate')
             task = Task ()
             task.task=tname
             task.ddate = ddate
-            task.save()
-        return redirect("/")
-        
+            if not task.task:
+                error_message = 'Task Name required'
+            elif not task.ddate:
+                error_message = 'Task Date Required'
+            else:
 
-class delete(View):
+                task.save()
 
-    def get(self,request,id):
+                return redirect("/")
+            
+            if error_message:
+                return render(request, 'add.html', {'error_message': error_message})
+            
+        return render(request, 'add.html')    
+
+   # Delete Method 
+    def delete (self,request,id):
         task =Task.objects.get(ID=id)
         task.delete()
 
         return redirect ('/')
+    
 
-class edit(View):
-    def get(self ,request,id):
-        tasks = Task.objects.get(ID=id)
-        return render(request,'edit.html',{'task':tasks})
+    #Edit Method / Update
+    
+    def edit(self ,request,id):
 
-    def post(self,request,id):
+        if request.method=="GET":
+         tasks = Task.objects.get(ID=id)
+         return render(request,'edit.html',{'task':tasks})
+
+        elif request.method=="POST":
        
-        task =Task.objects.get(ID=id)
-#       tID = request.POST.get('ID')
-        tname = request.POST.get('tname')
-        ddate = request.POST.get('ddate')
-        task.task=tname
-        task.ddate = ddate
-        task.save()
-        return redirect("/")
+            task =Task.objects.get(ID=id)
+    #       tID = request.POST.get('ID')
+            tname = request.POST.get('tname')
+            ddate = request.POST.get('ddate')
+            task.task=tname
+            task.ddate = ddate
+            task.save()
+            return redirect("/")
 
-        return render(request,'edit.html',{'task':task})
 
-class view(View):
-    def get(self,request,id):
-        tasks = Task.objects.get(id=id)
-        print(tasks)
-        return render(request, 'lists/view.html', {'tasks': tasks})    
+
+
+
+
+
+
+
+
+# class index(View):
+#     def get(self,request):
+#         tasks = Task.objects.all()
+#         return render(request,'index.html',{'tasks':tasks})
+
+# class add(View):
+#     def get(self,request):
+#      return render(request,'add.html')  
+    
+#     def post(self,request):
+#         if request.method=="POST":
+#             tname = request.POST.get('tname')
+#             ddate = request.POST.get('ddate')
+#             task = Task ()
+#             task.task=tname
+#             task.ddate = ddate
+#             if not task.task:
+#                 error_message = 'Task Name required'
+#             elif not task.ddate:
+#                 error_message = 'Task Date Required'
+#             else:
+
+#                 task.save()
+
+#                 return redirect("/")
+            
+#             if error_message:
+#                 return render(request, 'add.html', {'error_message': error_message})
+        
+
+# class delete(View):
+
+#     def get(self,request,id):
+#         task =Task.objects.get(ID=id)
+#         task.delete()
+
+#         return redirect ('/')
+
+# class edit(View):
+#     def get(self ,request,id):
+#         tasks = Task.objects.get(ID=id)
+#         return render(request,'edit.html',{'task':tasks})
+
+#     def post(self,request,id):
+       
+#         task =Task.objects.get(ID=id)
+# #       tID = request.POST.get('ID')
+#         tname = request.POST.get('tname')
+#         ddate = request.POST.get('ddate')
+#         task.task=tname
+#         task.ddate = ddate
+#         task.save()
+#         return redirect("/")
+
+#         return render(request,'edit.html',{'task':task})
+
+
+
+
+
+# # class view(View):
+# #     def get(self,request,id):
+# #         tasks = Task.objects.get(id=id)
+# #         print(tasks)
+# #         return render(request, 'lists/view.html', {'tasks': tasks})    
